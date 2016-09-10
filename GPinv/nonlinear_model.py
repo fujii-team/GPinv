f3230cfcba4951d7084dde904adf72e7fcae6ad6
fujiisoup <fujiisoup@gmail.com>
2016-09-10 19:55:13 +0900

    svgp added but not tested. The lisence and modification description is added.

--- GPinv/nonlinear_model.py
+++ GPinv/nonlinear_model.py
@@ -1,10 +1,13 @@
 from GPflow import gpmc
 from .vgp import TransformedVGP
 from .gpmc import TransformedGPMC
-
+from .svgp import TransformedSVGP
 
 class GPMC(TransformedGPMC):
     pass
 
 class VGP(TransformedVGP):
+    pass
+
+class SVGP(TransformedSVGP):
     pass
